1a770ea92db1e973db54fc6062642d59d568d621
chen.youwei <quickysharing@foxmail.com>
2023-08-31 23:56:00 +0800

    20230831：使用主账号测试push效果

--- app.js
+++ app.js
@@ -44,22 +44,6 @@ app.use('/logs', logRouter);
 app.use('/likes', likeRouter);
 app.use(registerRouter);
 
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler, uses the ejs template
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 app.use(function (err, req, res, next) {
   if (err) {
     res.status(err.status ? err.status : 500).json({
